c0a364b6313b2142ca3c774c12be2267f4576aa0
pratheekbhandary <30550942+pratheekbhandary@users.noreply.github.com>
2019-02-17 18:11:31 +0530

    commit 2:Adding Changes-1.js into dev-1 branch (#3)

---
+++ src/gitTryouts/change-1.js
@@ -0,0 +1,3 @@
+//This file was added by a developer after a similar file was added to master by amnother dev
+//So this should cause a merge conflict, if tried to merge with master 
+// But not merge conflict with git Tryout branch
