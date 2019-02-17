22b685a7d116427603e0b7b22907d5394de99f6e
pratheekbhandary <pratheekbhandary>
2019-02-17 18:17:46 +0530

    Commit-3.5

--- src/gitTryouts/change-1.js
+++ src/gitTryouts/change-1.js
@@ -1,3 +1,5 @@
 //This file was added by a developer after a similar file was added to master by amnother dev
 //So this should cause a merge conflict, if tried to merge with master 
 // But not merge conflict with git Tryout branch
+
+//This changes was done before commit4
